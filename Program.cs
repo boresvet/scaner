4426768abfddc8d2b856136ce05e70b6906afe49
”boresvet” <boresvet3123123@yandex.ru>
2021-07-09 19:55:23 +0300

    PointXY, Scan

--- Program.cs
+++ Program.cs
@@ -10,6 +10,13 @@ using System.Threading.Tasks;
 
 namespace Sick_test
 {
+    class PointXY{
+        public double X,Y;
+    }
+    class Scan{
+        public PointXY[] pointsArray; 
+        public DateTime time;
+    }
     class Program
     {
         const int Step = 286; //Количество шагов
@@ -34,28 +41,28 @@ namespace Sick_test
             //var dump = @"asciidump/scan_[--ffff-192.168.5.241]-2111_637563296658652353.bin";
             //var s = new FileStream(dump, FileMode.Open, FileAccess.Read);
             //var r = LMDScandataResult.Parse(s);
+            var t=Task.Run(()=>NewMethod("192.168.5.242"));
+            var y=Task.Run(()=>NewMethod("192.168.5.241"));
+            Task.WaitAll(t, y);
+        }
 
-            var lms = new LMS1XX("192.168.5.242", 2111, 5000, 5000);
+        public static void NewMethod(string addr)
+        {
+            var lms = new LMS1XX(addr, 2111, 5000, 5000);
 
             var Conv = new SpetialConvertor(40, -40, Step);
             lms.Connect();
 
             Thread.Sleep(100);
 
-
             var res = lms.LMDScandata2();
 
             double[] PolarScanResult = ConnectionResultDistanceData(res);
-            double[] Xpos = Conv.MakeX(PolarScanResult);
-            double[] Ypos = Conv.MakeY(PolarScanResult);
+            PointXY[] pos = Conv.MakePoint(PolarScanResult);
 
-            foreach(var d in Xpos) {
-                Console.Write($"{d}  ");
-            }
-            Console.WriteLine();
-            Console.WriteLine();
-            foreach(var d in Ypos) {
-                Console.Write($"{d}  ");
+            for (int i = 0; i < pos.Count(); i++)
+            {
+                Console.WriteLine($"{addr},  {i + 1}, {pos[i].X},  {pos[i].Y} ");
             }
             Console.WriteLine();
 
