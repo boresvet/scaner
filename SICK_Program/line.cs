b1e8143e0bf6b2337a9618391c6a9a4524e15fb7
boresvet <boresvet3123123@yandex.ru>
2022-10-05 23:30:43 +0300

    Save Json, with testsGen(not all, not work)

--- SICK_Program/line.cs
+++ SICK_Program/line.cs
@@ -1,3 +1,5 @@
+using System;
+
 namespace Sick_test
 {
     public struct line
@@ -8,9 +10,24 @@ namespace Sick_test
             A = (firstPoint.Y-secondPoint.Y)/(firstPoint.X-secondPoint.Y);
             B = (firstPoint.X*A) - firstPoint.Y;
         }
+        public void createLine(PointXY firstPoint, float Angle){
+            
+
+            var secondPoint = new PointXY(){X = firstPoint.X + 1000*Math.Cos(Angle*Math.PI/180), Y = firstPoint.X + 1000*Math.Sin(Angle*Math.PI/180)};
+            A = (firstPoint.Y-secondPoint.Y)/(firstPoint.X-secondPoint.Y);
+            B = (firstPoint.X*A) - firstPoint.Y;
+        }
         public void additionLines(line newline, int ratio){ //добавляет к структуре часть линии в размере 1/коэффециент
             A = A + (newline.A/ratio);
             B = B + (newline.B/ratio);
+        }
+        public PointXY pointIntersection(line firstline, line secondline){
+            var ret = new PointXY();
+
+
+
+
+            return ret;
         }
     }
 }
