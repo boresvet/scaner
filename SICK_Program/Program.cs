1206b8cc9d80296323902e1e3655b0066dca9c0a
boresvet <boresvet3123123@yandex.ru>
2023-04-20 17:46:24 +0300

    Lib Release

--- SICK_Program/Program.cs
+++ SICK_Program/Program.cs
@@ -10,5 +10,7 @@ namespace SickScanner
             var returns = new Sick_test.returns();
             Sick_test.SickScanners.RunScanners(returns);
         }
+
+        // dotnet publish -c Release -r linux-x64 -p:PublishSingleFile=true --self-contained=true -p:GenerateRuntimeConfigurationFiles=true -p:IncludeNativeLibrariesForSelfExtract=true
     }
 }
