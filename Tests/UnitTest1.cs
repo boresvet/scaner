80b5012951c402f517f774c132d2e198e6a92a08
boresvet <boresvet3123123@yandex.ru>
2023-05-05 23:14:11 +0300

    ReleaseWin

--- Tests/UnitTest1.cs
+++ Tests/UnitTest1.cs
@@ -14,7 +14,6 @@ namespace Sick_test
         int CarsInBuffer;
         int[] CarArray;
         config config;
-        IslandSeach scans;
         int? point1, point2;
         double? point3, point4;
         line[] testlinestexture;
@@ -31,34 +30,7 @@ namespace Sick_test
         public void Setup()
         {
 
-            var laserray = new line();
-            laserray.createLine(new PointXYint(){X = 5500, Y = 4500}, new PointXYint(){X = 4839, Y = 3749});
-            var cartexture = new line();
-            cartexture.createLine(new PointXYint(){X = 2050, Y = 1600}, new PointXYint(){X = 2150, Y = 100});
-            var roadtexture = new line();
-            roadtexture.createLine(new PointXYint(){X = -100, Y = 100}, new PointXYint(){X = 2400, Y = 100});
-            var scannerpoint = new PointXYint(){X = 5500, Y = 4500}; 
-            var tyu = laserray.firstPointIntersectionSegment(scannerpoint, laserray, new line[]{cartexture, roadtexture});
-            reallines = (tyu.Y == 652); 
 
-
-            //Тесты математики(типовое пересечение линий)
-            var line1 = new line();
-            line1.createLine(new PointXYint(){X = 0, Y = 10}, new PointXYint(){X = 10, Y = 10});
-            var line2 = new line();
-            line2.createLine(new PointXY(){X = 0, Y = 10}, new PointXY(){X = 10, Y = 10});
-            var ray1 = new line();
-            ray1.createLine(new PointXYint(){X = 0, Y = 0}, new PointXYint(){X = 10, Y = 20});
-            point1 = line1.DistancetopointSegment(new PointXYint(){X = 0, Y = 0}, ray1, line1);
-            var ray2 = new line();
-            ray2.createLine(new PointXYint(){X = 0, Y = 0}, new PointXYint(){X = 20, Y = 10});
-            point2 = line1.DistancetopointSegment(new PointXYint(){X = 0, Y = 0}, ray2, line1);
-            var ray3 = new line();
-            ray3.createLine(new PointXY(){X = 0, Y = 0}, new PointXY(){X = 10, Y = 20});
-            point3 = line2.DistancetopointSegment(new PointXY(){X = 0, Y = 0}, ray3, line2);
-            var ray4 = new line();
-            ray4.createLine(new PointXY(){X = 0, Y = 0}, new PointXY(){X = 20, Y = 10});
-            point4 = line2.DistancetopointSegment(new PointXY(){X = 0.0, Y = 0.0}, ray4, line2);
             //Console.WriteLine(point2);
 
             //Тест на первое пересечение
@@ -251,6 +223,35 @@ namespace Sick_test
         [Test]
         public void Maths()
         {
+            var laserray = new line();
+            laserray.createLine(new PointXYint(){X = 5500, Y = 4500}, new PointXYint(){X = 4839, Y = 3749});
+            var cartexture = new line();
+            cartexture.createLine(new PointXYint(){X = 2050, Y = 1600}, new PointXYint(){X = 2150, Y = 100});
+            var roadtexture = new line();
+            roadtexture.createLine(new PointXYint(){X = -100, Y = 100}, new PointXYint(){X = 2400, Y = 100});
+            var scannerpoint = new PointXYint(){X = 5500, Y = 4500}; 
+            var tyu = laserray.firstPointIntersectionSegment(scannerpoint, laserray, new line[]{cartexture, roadtexture});
+            reallines = (tyu.Y == 652); 
+
+
+            //Тесты математики(типовое пересечение линий)
+            var line1 = new line();
+            line1.createLine(new PointXYint(){X = 0, Y = 10}, new PointXYint(){X = 10, Y = 10});
+            var line2 = new line();
+            line2.createLine(new PointXY(){X = 0, Y = 10}, new PointXY(){X = 10, Y = 10});
+            var ray1 = new line();
+            ray1.createLine(new PointXYint(){X = 0, Y = 0}, new PointXYint(){X = 10, Y = 20});
+            point1 = line1.DistancetopointSegment(new PointXYint(){X = 0, Y = 0}, ray1, line1);
+            var ray2 = new line();
+            ray2.createLine(new PointXYint(){X = 0, Y = 0}, new PointXYint(){X = 20, Y = 10});
+            point2 = line1.DistancetopointSegment(new PointXYint(){X = 0, Y = 0}, ray2, line1);
+            var ray3 = new line();
+            ray3.createLine(new PointXY(){X = 0, Y = 0}, new PointXY(){X = 10, Y = 20});
+            point3 = line2.DistancetopointSegment(new PointXY(){X = 0, Y = 0}, ray3, line2);
+            var ray4 = new line();
+            ray4.createLine(new PointXY(){X = 0, Y = 0}, new PointXY(){X = 20, Y = 10});
+            point4 = line2.DistancetopointSegment(new PointXY(){X = 0.0, Y = 0.0}, ray4, line2);
+
             Assert.IsTrue((point1==11)&&(point2==null)&&((point3>=11.18)&&(point3<=11.1804))&&(point4==null)&&reallines);
         }
         //Тесты математики. Предыдущий - кооперация всех этих в один большой, чтобы галочка была только одна
